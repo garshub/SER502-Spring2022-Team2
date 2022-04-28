003c7ba0724c87b9f1d5f5a0a1c63176a6a57e7a
Manish <manish.m.r.96@gmail.com>
2022-04-28 00:40:29 -0700

    Runtime code refractor

--- src/main/java/runtimeUtility/Runtime.java
+++ src/main/java/runtimeUtility/Runtime.java
@@ -23,7 +23,6 @@ public class Runtime {
         while (programCounter < intermediateCode.size()) {
             programCounter = executeInstructionHandler(intermediateCode.get(programCounter), programCounter) + 1;
         }
-
     }
 
     private void initializeStackMemory() {
@@ -36,43 +35,25 @@ public class Runtime {
         String instructionType = instructions[0];
 
         switch (instructionType) {
-            case STORE_INSTRUCTION -> executeStoreInstruction(instructions); break;
-            case WRITE_INSTRUCTION -> executePrintInstruction(instructions); break;
-            case ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION -> executeArithmeticOperations(instructions); break;
-            case GT, GTE, LT, LTE, EQUAL_EQUAL, NOT_EQUAL  -> executeBooleanComparisonOperations(instructions); break;
-
-            case IF_START:
-                programCounter = executeIf(++programCounter);
-                break;
-            case ELSE_IF_START:
-                programCounter = executeElseIf(++programCounter);
-                break;
-            case ELSE_START:
-                programCounter = executeElse(++programCounter);
-                break;
-
-            case WHILE_START:
-                programCounter = executeWhile(++programCounter);
-                break;
-
-            case UNARY_MINUS:
-                executeUnaryMinusInstruction(instructions);
-                break;
-
-            case OR, AND -> executeBooleanInstruction(instructions); break;
-
-            default -> throw new IllegalStateException("Unexpected value: " + instructionType);
+            case STORE_INSTRUCTION -> executeStoreInstruction(instructions);
+            case WRITE_INSTRUCTION -> executePrintInstruction(instructions);
+            case ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION -> executeArithmeticOperations(instructions);
+            case GT, GTE, LT, LTE, EQUAL_TO, NOT_EQUAL_TO -> executeBooleanComparisonOperations(instructions);
+            case IF_START -> programCounter = executeIf(++programCounter);
+            case ELSE_IF_START -> programCounter = executeElseIf(++programCounter);
+            case ELSE_START -> programCounter = executeElse(++programCounter);
+            case WHILE_START -> programCounter = executeWhile(++programCounter);
+            case FOR_START -> programCounter = executeFor(++programCounter);
+            case UNARY_MINUS -> executeUnaryMinusInstruction(instructions);
+            case OR, AND -> executeBooleanInstruction(instructions);
         }
-
         return programCounter;
     }
 
-
-
     private void executeArithmeticOperations(String[] instruction) throws Exception {
 
-        DataType left = getWildCardValue(instruction[2]);
-        DataType right = getWildCardValue(instruction[3]);
+        DataType left = getDataValue(instruction[2]);
+        DataType right = getDataValue(instruction[3]);
         String leftDatatype = left.getDataType();
         String rightDatatype = right.getDataType();
 
@@ -95,69 +76,92 @@ public class Runtime {
 
     private void executeBooleanComparisonOperations(String[] instruction) throws Exception {
 
-
-
-        DataValues left = getWildCardValue(instruction[2]);
-        DataValues right = getWildCardValue(instruction[3]);
+        DataType left = getDataValue(instruction[2]);
+        DataType right = getDataValue(instruction[3]);
         String leftDatatype = left.getDataType();
         String rightDatatype = right.getDataType();
 
-        if(leftDatatype!=rightDatatype) {
+        if (leftDatatype != rightDatatype) {
             throw new Exception("Data mismatch");
-        }else if(leftDatatype==rightDatatype && !leftDatatype.equalsIgnoreCase("integer")) {
-            boolean leftOperand = getValue(instruction[2]).asBoolean();
-            boolean rightOperand = getValue(instruction[3]).asBoolean();
-            switch(instruction[0]) {
-                case EQUAL_EQUAL:
-                    setValue(instruction[1], new DataValues(leftOperand == rightOperand));
-                    break;
-                case NOT_EQUAL:
-                    setValue(instruction[1], new DataValues(leftOperand != rightOperand));
-                    break;
-                default:
-                    throw new Exception("Arithmetic exception Can't be performed on boolean type");
+        } else if (leftDatatype == rightDatatype && !leftDatatype.equalsIgnoreCase("integer")) {
+            boolean leftOperand = getValue(instruction[2]).dataAsBoolean();
+            boolean rightOperand = getValue(instruction[3]).dataAsBoolean();
+            switch (instruction[0]) {
+                case EQUAL_TO -> setValue(instruction[1], new DataType(leftOperand == rightOperand));
+                case NOT_EQUAL_TO -> setValue(instruction[1], new DataType(leftOperand != rightOperand));
+                default -> throw new Exception("Expression Type Error!");
             }
+        } else {
+            int leftOperand = getValue(instruction[2]).dataAsInteger();
+            int rightOperand = getValue(instruction[3]).dataAsInteger();
 
-        }
-        else {
-            int leftOperand = getValue(instruction[2]).asInt();
-            int rightOperand = getValue(instruction[3]).asInt();
-
-            switch(instruction[0]) {
-                case GT:
-                    setValue(instruction[1], new DataValues(leftOperand > rightOperand));
-                    break;
-                case GTE:
-                    setValue(instruction[1], new DataValues(leftOperand >= rightOperand));
-                    break;
-                case LT:
-                    setValue(instruction[1], new DataValues(leftOperand < rightOperand));
-                    break;
-                case LTE:
-                    setValue(instruction[1], new DataValues(leftOperand <= rightOperand));
-                    break;
-                case EQUAL_EQUAL:
-                    setValue(instruction[1], new DataValues(leftOperand == rightOperand));
-                    break;
-                case NOT_EQUAL:
-                    setValue(instruction[1], new DataValues(leftOperand != rightOperand));
-                    break;
+            switch (instruction[0]) {
+                case GT -> setValue(instruction[1], new DataType(leftOperand > rightOperand));
+                case GTE -> setValue(instruction[1], new DataType(leftOperand >= rightOperand));
+                case LT -> setValue(instruction[1], new DataType(leftOperand < rightOperand));
+                case LTE -> setValue(instruction[1], new DataType(leftOperand <= rightOperand));
+                case EQUAL_TO -> setValue(instruction[1], new DataType(leftOperand == rightOperand));
+                case NOT_EQUAL_TO -> setValue(instruction[1], new DataType(leftOperand != rightOperand));
             }
-
-
         }
+    }
+
+    private int executionBlock(int programCounter, String stopCondition, boolean skipLastConditionCheck) throws Exception {
+
+        while (programCounter >= 0) {
+            String instruction = intermediateCode.get(programCounter);
+            if (!instruction.equals(stopCondition)) {
+                if (!skipLastConditionCheck) {
+                    programCounter = executeInstructionHandler(instruction, programCounter);
+                }
+                programCounter = programCounter + 1;
+            } else {
+                break;
+            }
+        }
+        return programCounter;
+    }
+
+    private int executeIf(int programCounter) throws Exception {
+
+        programCounter = executionBlock(programCounter, CONDITION_END, false);
+        if (getValue(ACCUMULATOR_REGISTER).dataAsBoolean()) {
+            programCounter = executionBlock(programCounter, IF_END, false);
+            programCounter = executionBlock(programCounter, IF_ELSE_END, true);
+        } else {
+            programCounter = executionBlock(programCounter, IF_END, true);
+        }
+        return programCounter;
+    }
+
+
+    private int executeElse(int programCounter) throws Exception {
+        programCounter = executionBlock(programCounter, ELSE_END, false);
+        return programCounter;
+    }
+
+
+    private int executeElseIf(int programCounter) throws Exception {
+        programCounter = executionBlock(programCounter, CONDITION_END, false);
+        if (getValue(ACCUMULATOR_REGISTER).dataAsBoolean()) {
+            programCounter = executionBlock(programCounter, ELSE_IF_END, false);
+            programCounter = executionBlock(programCounter, IF_ELSE_END, true);
+        } else {
+            programCounter = executionBlock(programCounter, ELSE_IF_END, true);
+        }
+        return programCounter;
     }
 
     private void executeStoreInstruction(String[] instruction) {
         if (instruction[1].equals(ACCUMULATOR_REGISTER)) {
-            setValue(ACCUMULATOR_REGISTER, getWildCardValue(instruction[2]));
+            setValue(ACCUMULATOR_REGISTER, getDataValue(instruction[2]));
         } else {
             setValue(instruction[1], getValue(instruction[2]));
         }
     }
 
     private void executePrintInstruction(String[] instruction) {
-        DataType printData = getWildCardValue(instruction[1]);
+        DataType printData = getDataValue(instruction[1]);
 
         if (printData != null) {
             try {
@@ -176,7 +180,7 @@ public class Runtime {
         return this.output;
     }
 
-    private DataType getWildCardValue(String value) {
+    private DataType getDataValue(String value) {
         if (value.equals("NULL")) {
             return null;
         } else if (isInt(value)) {
@@ -190,7 +194,7 @@ public class Runtime {
         }
     }
 
-    private String cleanString(String value){
+    private String cleanString(String value) {
         int length = value.length();
         return value.substring(1, length - 1);
     }
@@ -228,111 +232,63 @@ public class Runtime {
         hashMap.put(identifier, value);
     }
 
-    private int executionBlock(int programCounter, String stopCond,boolean skipLastConditionCheck) throws Exception {
-        while(programCounter >= 0) {
-            String instruction = intermediateCode.get(programCounter);
-
-            if (instruction.equals(stopCond)) {
-                break;
-            }
-            else {
-                if(!skipLastConditionCheck) {
-                    programCounter = executeInstructionHandler(instruction, programCounter);
-                }
-                programCounter = programCounter +1;
-            }
-        }
-        return programCounter;
-    }
-
-    private int executeIf(int programCounter) throws Exception {
-
-        programCounter = executionBlock(programCounter, CONDITION_END,false);
-        if(getValue(ACCUMULATOR_REGISTER).asBoolean()){
-            programCounter = executionBlock(programCounter, IF_END,false);
-            programCounter = executionBlock(programCounter, IF_ELSE_END,true);
-        } else {
-            programCounter = executionBlock(programCounter, IF_END,true);
-        }
-        return programCounter;
-    }
-
-    private int executeElse(int programCounter) throws Exception {
-        programCounter = executionBlock(programCounter, ELSE_END,false);
-        return programCounter;
-    }
-
-    private int executeElseIf(int programCounter) throws Exception {
-        programCounter = executionBlock(programCounter, CONDITION_END,false);
-        if(getValue(ACCUMULATOR_REGISTER).asBoolean()){
-            programCounter = executionBlock(programCounter, ELSE_IF_END,false);
-            programCounter = executionBlock(programCounter, IF_ELSE_END,true);
-        } else {
-            programCounter = executionBlock(programCounter, ELSE_IF_END,true);
-        }
-        return programCounter;
-    }
-
     private int executeWhile(int whileStartCounter) throws Exception {
         int counter;
-
-        while(true) {
-            counter = executionBlock(whileStartCounter, CONDITION_END,false);
-
-            boolean b= getValue(ACCUMULATOR_REGISTER).asBoolean();
-
-            if(b) {
-
-                counter = executionBlock(counter, WHILE_END,false);
-
-            }
-            else {
-
-                counter = executionBlock(counter, WHILE_END,true);
+        do {
+            counter = executionBlock(whileStartCounter, CONDITION_END, false);
+            if (getValue(ACCUMULATOR_REGISTER).dataAsBoolean()) {
+                counter = executionBlock(counter, WHILE_END, false);
+            } else {
+                counter = executionBlock(counter, WHILE_END, true);
                 break;
             }
-        }
+        } while (true);
+
         return counter;
     }
 
-    private void executeUnaryMinusInstruction(String[] instruction) throws Exception{
-        DataValues operand = getWildCardValue(instruction[1]);
-        String operandDatatype = operand.getDataType();
-        if(operandDatatype.equalsIgnoreCase("INTEGER")) {
-            setValue(instruction[1], new DataValues(operand.asInt()*-1));
-        }else {
-            throw new Exception("Can not perform unary operation on binary");
-        }
+    private int executeFor(int forStartCounter) throws Exception {
+        int counter;
+        do {
+            counter = executionBlock(forStartCounter, CONDITION_END, false);
+            if (getValue(ACCUMULATOR_REGISTER).dataAsBoolean()) {
+                counter = executionBlock(counter, FOR_END, false);
+            } else {
+                counter = executionBlock(counter, FOR_END, true);
+                break;
+            }
+        } while (true);
 
+        return counter;
+    }
+
+    private void executeUnaryMinusInstruction(String[] instruction) throws Exception {
+        DataType operand = getDataValue(instruction[1]);
+        String operandDatatype = operand.getDataType();
+        if (operandDatatype.equalsIgnoreCase("INTEGER")) {
+            setValue(instruction[1], new DataType(-1 * operand.dataAsInteger()));
+        } else {
+            throw new Exception("Incompatible datatype");
+        }
     }
 
     private void executeBooleanInstruction(String[] instruction) throws Exception {
 
-        DataValues left = getWildCardValue(instruction[2]);
-        DataValues right = getWildCardValue(instruction[3]);
+        DataType left = getDataValue(instruction[2]);
+        DataType right = getDataValue(instruction[3]);
         String leftDatatype = left.getDataType();
         String rightDatatype = right.getDataType();
-        if(leftDatatype!=rightDatatype) {
+
+        if (leftDatatype != rightDatatype) {
             throw new Exception("Data mismatch");
-        }else if(leftDatatype==rightDatatype && !leftDatatype.equalsIgnoreCase("INTEGER")) {
-            boolean leftB = left.asBoolean();
-            boolean rightB = right.asBoolean();
+        } else if (leftDatatype == rightDatatype && !leftDatatype.equalsIgnoreCase("integer")) {
+            boolean leftB = left.dataAsBoolean();
+            boolean rightB = right.dataAsBoolean();
 
-            switch(instruction[0]) {
-
-                case AND:
-                    setValue(instruction[1], new DataValues(leftB && rightB));
-                    break;
-                case OR:
-                    setValue(instruction[1], new DataValues(leftB || rightB));
-                    break;
-
+            switch (instruction[0]) {
+                case AND -> setValue(instruction[1], new DataType(leftB && rightB));
+                case OR -> setValue(instruction[1], new DataType(leftB || rightB));
             }
         }
-
     }
-
-
-
-
 }
